c112a38dc94604700bb31f4bb833839545096d78
アラン <aymatsu00@gmail.com>
2025-06-05 19:35:14 +0000

    A borda da lista de produtos tem que estender até a borda da tela

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,8 +4,8 @@ import { ProductSearchTable } from '@/components/dashboard/ProductSearchTable';
 
 export default function DashboardPage() {
   return (
-    <div className="container mx-auto py-8 px-4 md:px-6">
-      <h1 className="text-3xl font-bold mb-8 font-headline text-center">
+    <div className="py-8">
+      <h1 className="text-3xl font-bold mb-8 font-headline text-center px-4 md:px-6">
         Busca de Produtos
       </h1>
       <ProductSearchTable />
