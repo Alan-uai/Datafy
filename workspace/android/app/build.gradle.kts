bec6d30eb758f7ddaae43315b0568df906e81a48
アラン <aymatsu00@gmail.com>
2025-06-08 20:08:21 +0000

    Para tornar os valores de configuração do arquivo google-services.json a

---
+++ workspace/android/app/build.gradle.kts
@@ -0,0 +1,29 @@
+// INSTRUÇÕES PARA CONFIGURAÇÃO DO GRADLE (NÍVEL DO MÓDULO DO APP)
+// Em seguida, no arquivo build.gradle.kts do módulo (nível do app),
+// adicione o plug-in google-services e todos os SDKs do Firebase que você quer usar no app:
+
+// plugins {
+//   id("com.android.application")
+//
+//   // Add the Google services Gradle plugin
+//   id("com.google.gms.google-services")
+//
+//   ...
+// }
+//
+// dependencies {
+//   // Import the Firebase BoM
+//   implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
+//
+//
+//   // TODO: Add the dependencies for Firebase products you want to use
+//   // When using the BoM, don't specify versions in Firebase dependencies
+//   implementation("com.google.firebase:firebase-analytics")
+//
+//
+//   // Add the dependencies for any other desired Firebase products
+//   // https://firebase.google.com/docs/android/setup#available-libraries
+// }
+
+// Nota: Este é um arquivo de placeholder. As configurações reais do Gradle
+// devem ser aplicadas em um projeto Android real.
---
+++ workspace/android/build.gradle.kts
@@ -0,0 +1,13 @@
+// INSTRUÇÕES PARA CONFIGURAÇÃO DO GRADLE (NÍVEL DO PROJETO)
+// Adicione o plug-in como uma dependência do arquivo build.gradle.kts no nível do projeto:
+
+// plugins {
+//   // ...
+//
+//   // Add the dependency for the Google services Gradle plugin
+//   id("com.google.gms.google-services") version "4.4.2" apply false
+//
+// }
+
+// Nota: Este é um arquivo de placeholder. As configurações reais do Gradle
+// devem ser aplicadas em um projeto Android real.
