60f7cf92b0ec1f681b05983ab1eaa585d1007faf
アラン <aymatsu00@gmail.com>
2025-07-10 19:12:46 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.button

--- src/components/dashboard/AttentionHorizonSelect.tsx
+++ src/components/dashboard/AttentionHorizonSelect.tsx
@@ -24,7 +24,7 @@ const AttentionHorizonSelect: React.FC<AttentionHorizonSelectProps> = ({
         onValueChange={(value) => onHorizonChange(parseInt(value, 10))}
         disabled={isLoading}
       >
-        <SelectTrigger className="w-auto border-none focus:ring-0 text-xl sm:text-2xl font-bold h-auto p-0">
+        <SelectTrigger className="w-auto border-none focus:ring-0 text-xl sm:text-2xl font-bold h-auto p-0 bg-transparent">
           <SelectValue placeholder="Selecione..." />
         </SelectTrigger>
         <SelectContent>
