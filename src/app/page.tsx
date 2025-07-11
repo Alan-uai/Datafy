fd0118cc10a37979633a9eb4c14c1305ea1e4a17
アラン <aymatsu00@gmail.com>
2025-07-11 11:06:17 +0000

    Mas a rota padrão agora é/dashboard, isso significa que a rota antiga nã

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-
-"use client";
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { LoadingSpinner } from '@/components/shared/LoadingSpinner';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace('/dashboard');
-  }, [router]);
-
-  return <LoadingSpinner text="REDIRECIONANDO" />;
+  redirect('/dashboard');
 }
